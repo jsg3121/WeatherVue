5591bab66bceef63e040f57ba784ee8d8c62a2c1
jsg3121 <xodm95@gmail.com>
2021-01-27 19:01:22 +0900

    FEAT(Project) : create project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Weather from "../views/Weather.vue";
+import About from "../views/About.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Weather",
+    component: Weather
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
   }
 ];
 
---
+++ src/views/Weather.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">
+    <h1>@decorator test</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {}
+})
+export default class Weather extends Vue {
+  setup() {
+    Mounted(() => {
+      console.log("It's Vue 3 wow!!!!!");
+    });
+  }
+}
+</script>
