b7bc8c1ba491f55cb0556ad9c24d9ce7e68662ec
jsg3121 <xodm95@gmail.com>
2021-04-27 17:14:11 +0900

    FEAT(test) : vue3 + serve API Test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div> -->
+  <div>
+    <h1>TEST</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { onMounted } from "vue";
+import http from "axios";
 
-@Options({
-  components: {
-    HelloWorld,
+export default {
+  setup(): void {
+    console.log("Component is mounted!");
+    onMounted(() => {
+      http
+        .get("https://best-weather-vue.herokuapp.com/api/weekly")
+        .then((res: unknown) => {
+          console.log(res);
+          return res;
+        });
+      console.log("Component is mounted!");
+    });
   },
-})
-export default class Home extends Vue {}
+};
 </script>
