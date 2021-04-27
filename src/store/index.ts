fdc9d0fd85b2d57ce77492059aa674f86ab655d4
jsg3121 <xodm95@gmail.com>
2021-04-27 13:40:09 +0900

    FEAT(client): weather-client update

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import { createStore } from "vuex";
-import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
   modules: {},
-  plugins: [createPersistedState()]
 });
