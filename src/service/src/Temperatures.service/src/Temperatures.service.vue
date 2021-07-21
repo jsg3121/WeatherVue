bbf60f64fc10c8746eed016c3ce06358ae4588f8
jsg3121 <xodm95@gmail.com>
2021-07-21 16:55:51 +0900

    FIX(domain) : 도메인으로 변경

--- src/service/src/Temperatures.service/src/Temperatures.service.vue
+++ src/service/src/Temperatures.service/src/Temperatures.service.vue
@@ -28,7 +28,7 @@ export default {
     const getGeolocation = async (position: any) => {
       await http
         .request({
-          url: "https://3.35.230.196:80/geolocation",
+          url: "https://best-weather.com/geolocation",
           method: "GET",
           params: {
             latitude: position.coords.latitude,
@@ -46,7 +46,7 @@ export default {
     const defaultGeolocation = async () => {
       await http
         .request({
-          url: "https://3.35.230.196:80/geolocation",
+          url: "https://best-weather.com/geolocation",
           method: "GET",
           params: {
             latitude: 37.514575,
